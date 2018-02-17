642d8c96ee94149bb007f80a6c474e9b064c2f88
Hadi Hidayat Hammurabi <didikipb007@gmail.com>
2018-02-17 18:03:08 +0700

    Get all todos and present that

--- client/src/components/Browse.vue
+++ client/src/components/Browse.vue
@@ -1,15 +1,50 @@
 <template>
   <div>
-    <h1>Browse your todo here.</h1>
+    <v-layout>
+      <v-flex xs6 offset-xs3>
+        <div class="white elevation-2">
+          <v-container>
+            sad
+          </v-container>
+        </div>
+      </v-flex>
+    </v-layout>
+    <br/>
+    <v-layout>
+      <v-flex xs6 offset-xs3>
+        <div class="white elevation-2">
+          <v-toolbar color="primary" dark>
+            <v-toolbar-title>Your Todo List</v-toolbar-title>
+          </v-toolbar>
+          <v-container>
+            <v-list v-for="todo in todos" :key="todo._id">
+              <v-checkbox
+                :label="todo.title"
+                @change="changeTodo(todo._id)"
+              ></v-checkbox>
+            </v-list>
+          </v-container>
+        </div>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
 <script>
-import { todo } from '../services';
+import services from '../services';
 
 export default {
-  mounted() {
-    console.log(todo);
+  data() {
+    return {
+      todos: {},
+    };
+  },
+  methods: {
+    // async changeTodo() {
+    // },
+  },
+  async mounted() {
+    this.todos = (await services.todo.get()).data;
   },
 };
 </script>
